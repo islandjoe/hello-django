77c0d8c13ab4feb1c9b570c0976b333206b395bd
Arthur Kho Caayon <islandjoe@gmail.com>
2019-10-07 09:15:19 +0300

    URLs, views, and unit tests

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
 
-# Create your views here.
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
 
-def homePageView(request):
-    return HttpResponse('Hello, Django!')
